8d66c39b1ee6fa4ba607b6d1594a41734dadd5bd
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-07-15 09:56:44 +0200

    add webapi and controllerclass

---
+++ graduationProjectBS/Linqin.Api/Controllers/InputsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linqin.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InputsController : ControllerBase
+{
+  public InputsController()
+  {
+
+  }
+
+  [HttpPost]
+
+}
