91d904cbbac5467b066bd963bb344d08f603d075
Ulfath023 <wali0023@gmail.com>
2022-07-15 10:11:32 +0200

    add GeometryShape model

---
+++ Linqin.Api/Models/GeometryShapes.cs
@@ -0,0 +1,26 @@
+namespace Linqin.Api.Models;
+
+public enum Shape
+{
+    Circle,
+    Square,
+    Triangle,
+    Number,
+    Letter
+}
+
+public enum Color
+{
+    Red,
+    Green,
+    Blue
+}
+
+
+public class GeometryShapes
+{
+    public int Id { get; set; }
+    public Shape Shape { get; set; }
+    public Color Color { get; set; }
+}
+
