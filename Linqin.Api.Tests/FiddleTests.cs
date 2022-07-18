8c1a4590a31bd75bfc23ab3ed895080e251f920c
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-07-18 10:15:38 +0200

    add test project and add package fluent assertions

---
+++ Linqin.Api.Tests/FiddleTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using FluentAssertions;
+
+namespace Linqin.Api.Tests;
+
+public class FiddleTests // controller, coderunner, fiddleclient
+{
+    [Fact]
+    public void fiddle_returns_string()
+    {
+        // Arrange
+        
+        // Act
+        // Assert
+    }
+}
