b9b033e1b964ece79374c2eec754e40f09e1679c
Ulfath023 <wali0023@gmail.com>
2022-07-18 11:02:51 +0200

    First test WiP

--- Linqin.Api.Tests/FiddleTests.cs
+++ Linqin.Api.Tests/FiddleTests.cs
@@ -1,16 +1,21 @@
 using Xunit;
 using FluentAssertions;
 
+
 namespace Linqin.Api.Tests;
 
 public class FiddleTests // controller, coderunner, fiddleclient
 {
     [Fact]
-    public void fiddle_returns_string()
+    public async void fiddle_returns_string()
     {
         // Arrange
-        
+        string listDef = "var test = int[]{7,2,8,3,6,3};";
+        string query = "test.OrderBy(x => x);";
+        var testService = new CodeRunner.CodeRunnerService();
         // Act
+        var result = await testService.RunLinqQueryOnList(listDef, query);
         // Assert
+        result.Should().Be("2,3,");
     }
 }
