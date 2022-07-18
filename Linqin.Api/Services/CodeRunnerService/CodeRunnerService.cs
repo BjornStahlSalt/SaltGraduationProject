edf5b45a0a1556336b97025a1ff72e1ab4ce2c85
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-07-18 12:18:50 +0200

    add test for tostringmethod

--- Linqin.Api/Services/CodeRunnerService/CodeRunnerService.cs
+++ Linqin.Api/Services/CodeRunnerService/CodeRunnerService.cs
@@ -1,35 +1,57 @@
 using Fiddle;
 using Fiddle.Exceptions;
+using Linqin.Api.Models;
 
 namespace CodeRunner;
 
 public class CodeRunnerService
 {
   private readonly FiddleClient _fiddleClient = new FiddleClient();
-  private string FormatPayload(string listDef, string linqQuery) =>
+
+  public string GetListAsString(List<GeometryShapes> listOfShapes)
+  {
+    var stringOfShapes = "new []{";
+
+    foreach (var shape in listOfShapes)
+    {
+      stringOfShapes += ($"new {{Shape = {shape.Shape}, Color = {shape.Color}, PriorityValue = {shape.PriorityValue}}}");
+      if (listOfShapes.IndexOf(shape) < listOfShapes.Count - 1)
+      {
+        stringOfShapes += ", ";
+      }
+    }
+
+    stringOfShapes += " };";
+    return stringOfShapes;
+  }
+
+  private string FormatPayload(string listDef, string linqQuery) => 
     "using System;" +
     "using System.Linq;" +
+    "using System.Collections.Generic;" +
     "public class Program {" +
     "public static void Main() {" +
     $"{listDef}" +
     $"var res = {linqQuery}" +
-    "Console.WriteLine(String.Join(&quot;, &quot;, res));}}";
+    "Console.WriteLine(String.Join(&quot;, &quot;, res));" +
+    "}}";
 
-  public async Task<string> RunLinqQueryOnList(string listDef, string query)
+  public async Task<string> RunLinqQueryOnList(List<GeometryShapes> listOfShapes, string query)
   {
+    var listDef = GetListAsString(listOfShapes);
     var code = FormatPayload(listDef, query);
     try
     {
       var fiddleResponse = await _fiddleClient.Run(code);
-      if(fiddleResponse.ConsoleOutput.Contains("error"))
+      if (fiddleResponse.ConsoleOutput.Contains("error"))
       {
         return fiddleResponse.ConsoleOutput.Split(':').Last();
       }
       return fiddleResponse.ConsoleOutput;
     }
-    catch(FiddleClientError)
+    catch (FiddleClientError ex)
     {
-      return "Uh oh! Something went wrong.";
+      return ex.Message;
     }
   }
 }
