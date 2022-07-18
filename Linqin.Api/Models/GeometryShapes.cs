47c95c507818f597eec003b470ca5a2b7ae71d6b
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-07-18 16:30:17 +0200

    add frontend connection to Linqin.Api

--- Linqin.Api/Models/GeometryShapes.cs
+++ Linqin.Api/Models/GeometryShapes.cs
@@ -2,10 +2,10 @@ namespace Linqin.Api.Models;
 
 public class GeometryShapes
 {
-    public int Id { get; set; }
+    // public int Id { get; set; }
     public string Shape { get; set; }
     public string Color { get; set; }
     public int PriorityValue { get; set; }
-    public string ImageUrl { get; set; }
+    // public string ImageUrl { get; set; }
 }
 
--- linqin.frontend/src/App.js
+++ linqin.frontend/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+
+        listOfShapes: [
+          { Shape: 'Circle', Color: 'Red', PriorityValue: 2 },
+          { Shape: 'Triangle', Color: 'Green', PriorityValue: 3 },
+          { Shape: 'Square', Color: 'Blue', PriorityValue: 1 }
+        ],
+        Query: 'test.OrderBy(s => s.PriorityValue);'
+      }),
+    };
+    fetch('https://localhost:7003/api/Inputs', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
