0bffd9c96b7d30c3c9df868f51fbbdedce0b7420
Ulfath023 <wali0023@gmail.com>
2022-07-20 17:06:07 +0200

    Add colors to shapes

--- linqin.frontend/src/Components/Collection/Collection.js
+++ linqin.frontend/src/Components/Collection/Collection.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Collection.css';
-import circle from '../../Images/Circle.svg'
+import circle from '../../Images/Circle.svg';
+import square from '../../Images/Square.svg';
+import triangle from '../../Images/Triangle.svg';
 
 
 function Collection({ shapes }) {
   return (
     <div>
-      <div className="Level__Collection">{shapes.map((s, i) =>
-        <img className="Level__Shape" key={i} src={circle} alt='A circle' />
-      )}
+      <div className="Level__Collection">{ shapes.map((s, i) =>
+        <img className={ "Level__Shape--" + s.color } key={ i } src={ circle } alt='A circle' />
+      ) }
       </div>
     </div>
   );
