968667990194f80e1a18654203bfc36b6c153278
Ulfath023 <wali0023@gmail.com>
2022-07-27 10:06:45 +0200

    Remove Shadows from cards

--- linqin.frontend/src/Components/Shape/Shape.js
+++ linqin.frontend/src/Components/Shape/Shape.js
@@ -5,7 +5,7 @@ import square from '../../Images/Square.svg';
 import ReactCardFlip from 'react-card-flip';
 import triangle from '../../Images/Triangle.svg';
 
-function Shape({ shape, shaded, large }) {
+function Shape({ shape, shaded }) {
 
   const chooseImage = () => {
     if (shape.shape === 'Circle') {
@@ -22,20 +22,23 @@ function Shape({ shape, shaded, large }) {
   };
   const [isFlipped, setIsFlipped] = useState(false);
   const handleClick = () => {
-    setIsFlipped(!isFlipped);
+    setIsFlipped(true);
+  };
+  const handleOtherClick = () => {
+    setIsFlipped(false);
   };
   return (
     <>
       <ReactCardFlip isFlipped={ isFlipped } flipDirection="horizontal">
         <div className='Shape__Card--Front'>
           <img className={ `Level__Shape--${shape.color} ${shaded}` } src={ chooseImage(shape) } alt='A circle' />
-          <button className='Shape__Card--FlipButton' onClick={ handleClick }></button>
+          <button className='Shape__Card--FlipButton' onPointerEnter={ handleClick }></button>
         </div>
         <div className='Shape__Card--Back'>
           <img className={ `Level__Shape--${shape.color} large` } src={ chooseImage(shape) } alt='A circle' />
           <p className='Properties__Property'>.Shape = "{ shape.shape }"</p>
           <p className='Properties__Property'>.Color = "{ shape.color }"</p>
-          <button className='Shape__Card--FlipButton' onClick={ handleClick }></button>
+          <button className='Shape__Card--FlipButton' onPointerLeave={ handleOtherClick }></button>
         </div>
       </ReactCardFlip>
     </>
